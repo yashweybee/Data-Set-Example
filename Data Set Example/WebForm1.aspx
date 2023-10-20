f02ab8c60a7a26170a335d6b1f8c5d91e92d6232
yashweybee <yashparmar@weybee.in>
2023-10-20 14:38:53 +0530

    Add project files.

---
+++ Data Set Example/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Data_Set_Example.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" ForeColor="White">
+                <EditRowStyle BackColor="#3333FF" />
+                <HeaderStyle BackColor="#3366FF" BorderColor="White" BorderStyle="Solid" />
+                <RowStyle BackColor="#33CCFF" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Data Set Example/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Data_Set_Example
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //using (SqlConnection conn = new SqlConnection("data source=.; database=Employees; integrated security=SSPI"))
+            //{
+            //    SqlDataAdapter sda = new SqlDataAdapter("select * from emp", conn);
+            //    DataSet ds = new DataSet();
+            //    sda.Fill(ds);
+            //    GridView1.DataSource = ds;
+            //    GridView1.DataBind();
+            //}
+
+        }
+    }
+}
